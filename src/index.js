4102cb5e2208f8a08f18ce6cd6772f1ed5f6bb82
Craig Warren <craig@craigwarren.co.uk>
2018-04-22 12:41:32 +0100

    Add logic to get important information from nest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 var request = require("request");
 var fs = require('fs')
 
-fs.readFile('/run/secrets/nest_token', 'utf8', function (err,data) {
+var handleNestResponse = function(error, response, body) {
+    if (error) throw new Error(error);
+
+    var response = {};
+    var thermostats = JSON.parse(body).devices.thermostats;
+    Object.keys(thermostats).forEach( function(key) {
+        var thermostat = thermostats[key];
+        response[thermostat.name] = {
+            ambient_temperature_c: thermostat.ambient_temperature_c,
+            target_temperature_c: thermostat.target_temperature_c,
+            hvac_state: thermostat.hvac_state
+        }
+    });
+    console.log(response);
+};
+
+var requestNestData = function(err, data) {
     var token;
     if (err) {
         console.log("/run/secrets/nest_token not found");
@@ -10,7 +26,6 @@ fs.readFile('/run/secrets/nest_token', 'utf8', function (err,data) {
         token = data.trim();
     }
 
-    console.log(token);
     var options = {
         method: 'GET',
         url: 'https://developer-api.nest.com/',
@@ -21,8 +36,7 @@ fs.readFile('/run/secrets/nest_token', 'utf8', function (err,data) {
         }
     };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log(body);
-    });
-});
+    request(options, handleNestResponse);
+};
+
+fs.readFile('/run/secrets/nest_token', 'utf8', requestNestData);
