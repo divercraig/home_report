8057646aa4c4f9b3f719d87291eb2faa03313ae7
Craig Warren <craig@craigwarren.co.uk>
2018-04-23 16:27:20 +0100

    Parameterising the influxdb details into the home_report container

--- src/index.js
+++ src/index.js
@@ -1,37 +1,76 @@
 var request = require("request");
 var fs = require('fs')
 
+var influx_host;
+var influx_port;
+var influx_user;
+var influx_password;
+var nest_token;
+
+
+var initInflux = function() {
+    const Influx = require('influx');
+    const influx = new Influx.InfluxDB({
+     host: process.env.INFLUXDB_HOST,
+     port: process.env.INFLUXDB_PORT,
+     database: "home_report",
+     username: process.env.INFLUXDB_USER,
+     password: influx_password,
+     schema: [
+       {
+         measurement: "thermostat",
+         fields: {
+           ambient_temperature_c: Influx.FieldType.FLOAT,
+           target_temperature_c: Influx.FieldType.FLOAT,
+           hvac_state: Influx.FieldType.INTEGER
+         },
+         tags: [
+           "name"
+         ]
+       }
+     ]
+ });
+
+ return influx;
+}
+
+var logToInflux = function(metrics) {
+    var influx = initInflux();
+    influx.writePoints(metrics).then(
+        () => {console.log("Write to InfluxDB complete");},
+        (err) => {console.log("Failed to write to InfluxDb: " + err);}
+    );
+};
+
 var handleNestResponse = function(error, response, body) {
     if (error) throw new Error(error);
 
-    var response = {};
+    var metrics = [];
     var thermostats = JSON.parse(body).devices.thermostats;
     Object.keys(thermostats).forEach( function(key) {
         var thermostat = thermostats[key];
-        response[thermostat.name] = {
-            ambient_temperature_c: thermostat.ambient_temperature_c,
-            target_temperature_c: thermostat.target_temperature_c,
-            hvac_state: thermostat.hvac_state
-        }
+        metrics.push({
+            measurement: "thermostat",
+            fields: {
+                ambient_temperature_c: thermostat.ambient_temperature_c,
+                target_temperature_c: thermostat.target_temperature_c,
+                hvac_state: thermostat.hvac_state === "heating" ? 1 : 0
+            },
+            tags: {
+                name: thermostat.name
+            }
+        });
     });
-    console.log(response);
+    logToInflux(metrics);
 };
 
-var requestNestData = function(err, data) {
-    var token;
-    if (err) {
-        console.log("/run/secrets/nest_token not found");
-        token = process.env.NEST_TOKEN
-    } else {
-        token = data.trim();
-    }
-
+var requestNestData = function() {
     var options = {
         method: 'GET',
         url: 'https://developer-api.nest.com/',
         headers: {
             'cache-control': 'no-cache',
-            'authorization': token,
+            'authorization': nest_token,
             'content-type': 'application/json'
         }
     };
@@ -39,4 +78,44 @@ var requestNestData = function(err, data) {
     request(options, handleNestResponse);
 };
 
-fs.readFile('/run/secrets/nest_token', 'utf8', requestNestData);
+const initialise = function() {
+    try{
+        nest_token =
+            fs.readFileSync('/run/secrets/nest_token', 'utf8').trim();
+    } catch(e) {
+        console.log("unable to read /run/secrets/nest_token: " + e);
+        throw e;
+    }
+
+    try{
+        influx_password =
+            fs.readFileSync('/run/secrets/influx_pass', 'utf8').trim();
+    } catch(e) {
+        console.log("unable to read /run/secrets/influx_pass: " + e);
+        throw e;
+    }
+
+    if (process.env.INFLUXDB_HOST) {
+        influx_host = process.env.INFLUXDB_HOST;
+    } else {
+        console.log("Missing env variable:INFLUXDB_HOST");
+        throw new exception("Missing env variable:INFLUXDB_HOST");
+    }
+
+    if (process.env.INFLUXDB_PORT) {
+        influx_port = process.env.INFLUXDB_PORT;
+    } else {
+        console.log("Missing env variable:INFLUXDB_PORT");
+        throw new exception("Missing env variable:INFLUXDB_PORT");
+    }
+
+    if (process.env.INFLUXDB_USER) {
+        influx_user = process.env.INFLUXDB_USER;
+    } else {
+        console.log("Missing env variable:INFLUXDB_USER");
+        throw new exception("Missing env variable:INFLUXDB_USER");
+    }
+};
+
+initialise();
+requestNestData();
